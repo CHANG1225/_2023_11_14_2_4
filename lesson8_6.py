38c368e31ba1e6e9c14bb4e6d018c892f16cdece
CHANG1225 <150831206+CHANG1225@users.noreply.github.com>
2023-12-08 10:56:41 +0000

    hgfd

---
+++ lesson8_6.py
@@ -0,0 +1,33 @@
+import pyinputplus as pypi
+
+def getBMI(hight:float,weight:float)->tuple[float,str]:
+    bmi = weight / (hight/100)**2
+    message=''
+
+    if bmi >=35:
+        message='「您重度肥胖」'
+
+    elif bmi >=30 and bmi <35:
+        message='「您中度肥胖」'
+
+    elif bmi >=27 and bmi <30:
+        message='「您輕度肥胖」'
+
+    elif bmi >=24 and bmi <27:    
+        message='「您的體重過重」'
+
+    elif bmi >=18.5 and bmi<24:
+        message='「您的體重屬正常範圍」'
+
+    else:
+        message='「您的體重過輕」'
+    return(bmi,message)
+
+weight = pypi.inputFloat('請輸入體重,單位為(公斤):',min=0)
+print(weight)
+hight = pypi.inputFloat('請輸入身高,單位為(公分):',min=0)
+print(hight)
+
+
+bmi,message =getBMI(hight=hight,weight=weight)
+print(f'您的bmi是{bmi:.2f},{message}')
