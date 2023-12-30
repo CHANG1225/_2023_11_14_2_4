418c6344d303fd294f7f8a3d0b37c5005adce682
CHANG1225 <150831206+CHANG1225@users.noreply.github.com>
2023-12-30 16:48:13 +0000

    modify

--- lesson11_1/main.py
+++ lesson11_1/main.py
@@ -1,50 +1,12 @@
-import random
-import csv
 import pyinputplus as pyip
-
-def getStudents(student_nums:int=1, scores_nums:int=2) -> list[list]:
-    '''
-    參數: student_nums -> 學生人數\n
-    參數: scores_nums -> 科目數\n
-    '''
-    with open('names.txt',mode='r',encoding='utf-8') as file:
-        names:str = file.read()
-
-    nameList:list[str] = names.split('\n')
-    students:list[list] = []
-
-    names:list[str] = random.choices(nameList,k=student_nums)
-    for name in names:
-        stu:list[int|str] = []
-        stu.append(name)    
-        for i in range(scores_nums):
-            stu.append(random.randint(40,100))
-        students.append(stu)
-
-    return students
-
-def saveToCSV(fileName:str,data:list[list],subjects_nums:int)-> bool:
-    fileName +='.csv'
-    subjects = [f'科目{i+1}'for i in range(subjects_nums)]
-    fields =['姓名']
-    fields.extend(subjects)
-    with open(fileName,mode='w',encoding='utf-8',newline='') as file:
-        try:
-            writer = csv.writer(file)
-            #fields = None
-            writer.writerow(fields)
-            writer.writerows(data)
-        except:
-            return False
-        else:
-            return True
+from tools import getStudents,saveToCSV
 
 if __name__ == '__main__':
     s_nums:int = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
     o_nums:int = pyip.inputInt("請輸入科目數(1~7):",min=1,max=7)
     students:list[list] = getStudents(student_nums=s_nums,scores_nums=o_nums)
-    fileName=pyip.inputFilename('請輸入檔案名稱(不用輸入副檔名):')
-    if saveToCSV(fileName=fileName,data=students,subjects_nums=o_nums):
-        print('存檔成功')
+    fileName = pyip.inputFilename("請輸入檔案名稱(不用輸入副檔名稱):")
+    if saveToCSV(fileName=fileName,data=students,subject_nums=o_nums):
+        print("存檔成功")
     else:
-        print('存檔失敗')
+        print("存檔失敗")
