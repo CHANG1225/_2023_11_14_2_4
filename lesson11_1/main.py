a742b298e13c1ff926e2e38cafa2bd750988b8f5
CHANG1225 <150831206+CHANG1225@users.noreply.github.com>
2023-12-30 16:42:50 +0000

    modify

--- lesson11_1/main.py
+++ lesson11_1/main.py
@@ -23,15 +23,28 @@ def getStudents(student_nums:int=1, scores_nums:int=2) -> list[list]:
 
     return students
 
-def saveToCSV(fileName:str,data:list[list])-> None:
+def saveToCSV(fileName:str,data:list[list],subjects_nums:int)-> bool:
     fileName +='.csv'
+    subjects = [f'科目{i+1}'for i in range(subjects_nums)]
+    fields =['姓名']
+    fields.extend(subjects)
     with open(fileName,mode='w',encoding='utf-8',newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(data)
+        try:
+            writer = csv.writer(file)
+            #fields = None
+            writer.writerow(fields)
+            writer.writerows(data)
+        except:
+            return False
+        else:
+            return True
 
 if __name__ == '__main__':
     s_nums:int = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
     o_nums:int = pyip.inputInt("請輸入科目數(1~7):",min=1,max=7)
     students:list[list] = getStudents(student_nums=s_nums,scores_nums=o_nums)
     fileName=pyip.inputFilename('請輸入檔案名稱(不用輸入副檔名):')
-    saveToCSV(fileName=fileName,data=students)
+    if saveToCSV(fileName=fileName,data=students,subjects_nums=o_nums):
+        print('存檔成功')
+    else:
+        print('存檔失敗')
